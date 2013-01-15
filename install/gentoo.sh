afa54159fac33b6a56c5aabd8dd01d8cc0465bb8
Vital Kudzelka <vital.kudzelka@gmail.com>
2013-01-15 13:52:33 +0300

    fix sed substitution error

--- install/gentoo.sh
+++ install/gentoo.sh
@@ -5,6 +5,6 @@
 wget https://raw.github.com/vitalk/serv-recipes/master/nginx/app -O /etc/nginx/sites-available/$my_app
 ln -sf /etc/nginx/sites-{available,enabled}/$my_app
 
-sed -i 's/YOUR_APP_NAME/$my_app/' /etc/nginx/sites-available/$my_app
+sed -i "s/YOUR_APP_NAME/$my_app/" /etc/nginx/sites-available/$my_app
 sed -i 's/YOUR_SERVER_IP:80/80/' /etc/nginx/sites-available/$my_app
-sed -i 's/YOUR_SERVER_FQDN/$my_domain/' /etc/nginx/sites-available/$my_app
+sed -i "s/YOUR_SERVER_FQDN/$my_domain/" /etc/nginx/sites-available/$my_app
