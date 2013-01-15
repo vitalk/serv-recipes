93c1c30474a6895a92e53c6135458b8c1c4cb173
Vital Kudzelka <vital.kudzelka@gmail.com>
2013-01-15 13:41:23 +0300

    remove existing destination files while create symbolic link

--- install/gentoo.sh
+++ install/gentoo.sh
@@ -3,7 +3,7 @@
 # Usage: curl https://raw.github.com/vitalk/serv-recipes/master/install/gentoo.sh | my_app=app my_domain=app.example.com sh
 
 wget https://raw.github.com/vitalk/serv-recipes/master/nginx/app -O /etc/nginx/sites-available/$my_app
-ln -s /etc/nginx/sites-{available,enabled}/$my_app
+ln -sf /etc/nginx/sites-{available,enabled}/$my_app
 
 sed -i 's/YOUR_APP_NAME/$my_app/' /etc/nginx/sites-available/$my_app
 sed -i 's/YOUR_SERVER_IP:80/80/' /etc/nginx/sites-available/$my_app
