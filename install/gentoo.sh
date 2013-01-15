7fdf236e2079418c09422cd7bf7d91a9777f7d2d
Vital Kudzelka <vital.kudzelka@gmail.com>
2013-01-15 13:39:49 +0300

    fix typos

--- install/gentoo.sh
+++ install/gentoo.sh
@@ -3,8 +3,8 @@
 # Usage: curl https://raw.github.com/vitalk/serv-recipes/master/install/gentoo.sh | my_app=app my_domain=app.example.com sh
 
 wget https://raw.github.com/vitalk/serv-recipes/master/nginx/app -O /etc/nginx/sites-available/$my_app
-ln -s /etc/nginx/sites-{avaliable,enabled}/$my_app
+ln -s /etc/nginx/sites-{available,enabled}/$my_app
 
-sed -i 's/YOUR_APP_NAME/$my_app/' /etc/nginx/sites-avaliable/$my_app
-sed -i 's/YOUR_SERVER_IP:80/80/' /etc/nginx/sites-avaliable/$my_app
-sed -i 's/YOUR_SERVER_FQDN/$my_domain/' /etc/nginx/sites-avaliable/$my_app
+sed -i 's/YOUR_APP_NAME/$my_app/' /etc/nginx/sites-available/$my_app
+sed -i 's/YOUR_SERVER_IP:80/80/' /etc/nginx/sites-available/$my_app
+sed -i 's/YOUR_SERVER_FQDN/$my_domain/' /etc/nginx/sites-available/$my_app
