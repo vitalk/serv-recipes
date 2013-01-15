8c045d128f6241dcc7e5169ced03acbdabdd61bf
Vital Kudzelka <vital.kudzelka@gmail.com>
2013-01-15 13:20:14 +0300

    initial commit

---
+++ install/gentoo.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Performs a complete installation of new nginx site for gentoo server
+# Usage: curl https://raw.github.com/vitalk/serv-recipes/master/install/gentoo.sh | my_app=app my_domain=app.example.com sh
+
+wget https://raw.github.com/vitalk/serv-recipes/master/nginx/app -O /etc/nginx/sites-available/$my_app
+ln -s /etc/nginx/sites-{avaliable,enabled}/$my_app
+
+sed -i 's/YOUR_APP_NAME/$my_app/' /etc/nginx/sites-avaliable/$my_app
+sed -i 's/YOUR_SERVER_IP:80/80/' /etc/nginx/sites-avaliable/$my_app
+sed -i 's/YOUR_SERVER_FQDN/$my_domain/' /etc/nginx/sites-avaliable/$my_app
