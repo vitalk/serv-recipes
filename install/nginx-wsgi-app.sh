917c2931582b3b3a40a00345094fa07676b89524
Vital Kudzelka <vital.kudzelka@gmail.com>
2013-04-01 19:15:32 +0300

    more clear filename
    this is example of configuration WSGI server behind NGINX proxy

--- install/nginx-wsgi-app.sh
+++ install/nginx-wsgi-app.sh
@@ -2,7 +2,7 @@
 # Performs a complete installation of new wsgi app behind nginx proxy
 # Usage: curl https://raw.github.com/vitalk/serv-recipes/master/install/nginx-wsgi-app.sh | my_app=app my_domain=app.example.com sh
 
-wget https://raw.github.com/vitalk/serv-recipes/master/nginx/app -O /etc/nginx/sites-available/$my_app
+wget https://raw.github.com/vitalk/serv-recipes/master/nginx/wsgi-app -O /etc/nginx/sites-available/$my_app
 ln -sf /etc/nginx/sites-{available,enabled}/$my_app
 
 sed -i "s/YOUR_APP_NAME/$my_app/" /etc/nginx/sites-available/$my_app
