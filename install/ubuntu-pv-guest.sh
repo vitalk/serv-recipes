a88439d207a64fd89570bea87a202003b78ce1a5
Vital Kudzelka <vital.kudzelka@gmail.com>
2013-04-01 14:48:20 +0300

    replace my_domu_name to my_domu_fqdn
    Now my naming convention is more clean:
        * each PV host store own config at file named as host FQDN

--- install/ubuntu-pv-guest.sh
+++ install/ubuntu-pv-guest.sh
@@ -2,7 +2,7 @@
 # Create a ubuntu PV guest VM
 #
 # Note: list of the official ubuntu mirrors available on https://launchpad.net/ubuntu/+archivemirrors
-# Usage: curl https://raw.github.com/vitalk/serv-recipes/master/install/ubuntu-pv-guest.sh | my_mirror=http://ftp.byfly.by/ubuntu my_domu_name=ubuntu my_vif_ip=192.168.1.99 my_vif_mac=00:16:3E:E0:4D:50 my_domu_memory=256 sh
+# Usage: curl https://raw.github.com/vitalk/serv-recipes/master/install/ubuntu-pv-guest.sh | my_mirror=http://ftp.byfly.by/ubuntu my_domu_fqdn=example.com my_vif_ip=192.168.1.99 my_vif_mac=00:16:3E:E0:4D:50 my_domu_memory=256 sh
 
 # Usage: hex [<string>]
 # return the unique hex string to generate valid MAC address 00:16:3E:xx:xx:xx
@@ -19,16 +19,16 @@ mac() {
 }
 
 # setup the initial guest configuration
-if [ -f /etc/xen/$my_domu_name.cfg ]; then
-    echo "The domU with name $my_domu_name already exists. Exiting..."
+if [ -f /etc/xen/$my_domu_fqdn.cfg ]; then
+    echo "The domU with name $my_domu_fqdn already exists. Exiting..."
     exit 0
 fi
-wget https://raw.github.com/vitalk/serv-recipes/master/xen/ubuntu.cfg -O /etc/xen/$my_domu_name.cfg
-ln -s /etc/xen/$my_domu_name.cfg /etc/xen/auto/$my_domu_name.cfg
+wget https://raw.github.com/vitalk/serv-recipes/master/xen/ubuntu.cfg -O /etc/xen/$my_domu_fqdn.cfg
+ln -s /etc/xen/$my_domu_fqdn.cfg /etc/xen/auto/$my_domu_fqdn.cfg
 
-sed -i "s/YOUR_DOMU_NAME/$my_domu_name/" /etc/xen/$my_domu_name.cfg
-sed -i "s/YOUR_DOMU_MEMORY/$my_domu_memory/" /etc/xen/$my_domu_name.cfg
-sed -i "s/YOUR_VIF_IP/$my_vif_ip/" /etc/xen/$my_domu_name.cfg
+sed -i "s/YOUR_DOMU_NAME/$my_domu_fqdn/" /etc/xen/$my_domu_fqdn.cfg
+sed -i "s/YOUR_DOMU_MEMORY/$my_domu_memory/" /etc/xen/$my_domu_fqdn.cfg
+sed -i "s/YOUR_VIF_IP/$my_vif_ip/" /etc/xen/$my_domu_fqdn.cfg
 
 # generate unique MAC if it's not set explicitly
-sed -i "s/YOUR_VIF_MAC/${my_vif_mac:-$(mac $my_domu_name)}/" /etc/xen/$my_domu_name.cfg
+sed -i "s/YOUR_VIF_MAC/${my_vif_mac:-$(mac $my_domu_fqdn)}/" /etc/xen/$my_domu_fqdn.cfg
