9f530f382eaf48deb94164acf224775b5e9add26
Vital Kudzelka <vital.kudzelka@gmail.com>
2013-04-02 11:30:12 +0300

    sh does not understand variable expansion use bash on sample

--- install/nginx-wsgi-app.sh
+++ install/nginx-wsgi-app.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Performs a complete installation of new wsgi app behind nginx proxy
-# Usage: curl https://raw.github.com/vitalk/serv-recipes/master/install/nginx-wsgi-app.sh | my_app=app my_domain=app.example.com sh
+# Usage: curl https://raw.github.com/vitalk/serv-recipes/master/install/nginx-wsgi-app.sh | my_app=app my_domain=app.example.com bash
 
 mkdir -p /etc/nginx/sites-{available,enabled}
 wget https://raw.github.com/vitalk/serv-recipes/master/nginx/wsgi-app -O /etc/nginx/sites-available/$my_app
-ln -sf /etc/nginx/sites-{available,enabled}/$my_app
+ln -s /etc/nginx/sites-{available,enabled}/$my_app
 
 sed -i "s/YOUR_APP_NAME/$my_app/" /etc/nginx/sites-available/$my_app
 sed -i 's/YOUR_SERVER_IP:80/80/' /etc/nginx/sites-available/$my_app
